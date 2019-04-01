e4635e0f57a53e5b5c312595bc05c4eeb0c90c54
Laura Brumm <lpocho13@gmail.com>
2019-04-01 08:34:52 -0500

    reorganize file structure

---
+++ source/src/js/main/main.js
@@ -0,0 +1,14 @@
+(function($, global) {
+    var laurabrumm = {
+        // Initialize everything
+        init: function() {
+            
+        }
+    }
+
+    // Add window object
+    global.laurabrumm = laurabrumm;
+
+    // Run it
+    laurabrumm.init();
+})(jQuery, window);
